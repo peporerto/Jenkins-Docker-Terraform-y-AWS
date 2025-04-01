c90f37a7d59801a3f47933005ae5f44d6b35ad85
peporerto <santiagog2142@gmail.com>
2025-03-31 20:45:49 -0400

    fix it2

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # ID de una AMI (Amazon Machine Image) válida
+  ami           = "ami-071226ecf16aa7d96"  # ID de AMI de Amazon Linux 2023
   instance_type = "t2.micro"  # Tipo de instancia (puedes ajustarlo según tus necesidades)
 
   tags = {
